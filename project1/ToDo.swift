531259d12666467dbfbb607e24b4f8f66ac77ea2
CamVelaa <153161977+CamVelaa@users.noreply.github.com>
2024-06-08 17:15:15 -0400

    create ToDo

---
+++ project1/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  toDo.swift
+//  project1
+//
+//  Created by Cameron Vela on 6/8/24.
+//
+
+import Foundation
+
+class ToDo {
+    var name: String
+    var description: String
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+}

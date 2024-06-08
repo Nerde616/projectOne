1cb2b2968501680cc83601e3e5e092301d7bf5ed
Nerde616 <147065979+Nerde616@users.noreply.github.com>
2024-06-08 16:52:06 -0400

    Initial commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Earnest   Trice on 6/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
